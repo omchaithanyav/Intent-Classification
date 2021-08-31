44e9fc8693e4e3d3b6f7f4855e66c365a7a3ed8d
Om Chaithanya V <45726271+omchaithanyav@users.noreply.github.com>
2021-08-31 21:15:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-from tensorflow import keras
+import keras
+from keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
 
@@ -19,20 +20,13 @@ def classify():
     Tokenizer = pickle.load(open('models/tokenizer.pkl', 'rb'))
 
     tokens = Tokenizer.texts_to_sequences([text])
-
+    tokens = pad_sequences(tokens, maxlen=100)
     prediction = saved_model.predict(np.array(tokens))
     pred = np.argmax(prediction)
     classes = ['BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook']
     result = classes[pred]
 
     return render_template("result.html", output = "The intent of the user is {}".format(result))
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
