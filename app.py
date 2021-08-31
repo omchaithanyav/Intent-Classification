e26ac4a86d24924e0ee739be612908cc316bb480
Om Chaithanya V <45726271+omchaithanyav@users.noreply.github.com>
2021-08-31 20:09:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    text = str(request.form['sentence'])
+
+    # load the saved model and tokenizer
+    saved_model = keras.models.load_model('models/Intent_Classification.h5')
+    Tokenizer = pickle.load(open('models/tokenizer.pkl', 'rb'))
+
+    tokens = Tokenizer.texts_to_sequences([text])
+    tokens = pad_sequences(tokens, maxlen=100)
+    prediction = saved_model.predict(np.array(tokens))
+    pred = np.argmax(prediction)
+    classes = ['BookRestaurant', 'GetWeather', 'PlayMusic', 'RateBook']
+    result = classes[pred]
+
+    return render_template("result.html", output = "The intent of the user is {}".format(result))
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5544)
+
